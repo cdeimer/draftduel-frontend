7476692e09705546951b6aaae47735aaee7d3f9e
cdeimer <cullendeimer@gmail.com>
2024-05-05 23:55:37 -0400

    finish refactoring roster component

--- src/app/components/roster/roster.component.ts
+++ src/app/components/roster/roster.component.ts
@@ -1,21 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { PlayerPool } from '../../services/stats.service';
 import { RosterEntryComponent } from '../roster-entry/roster-entry.component';
-import { NgForOf, NgIf } from '@angular/common';
+import { CommonModule, NgFor, NgForOf, NgIf } from '@angular/common';
 import { GameState } from '../../services/game-state.service';
 import { EmptyRosterEntryComponent } from '../roster-entry/empty-roster-entry/empty-roster-entry.component';
 import { Roster } from '../../classes/roster';
+import { Position } from '../../interfaces/position';
 
 @Component({
   selector: 'app-roster',
   standalone: true,
-  imports: [RosterEntryComponent, EmptyRosterEntryComponent, NgForOf],
+  imports: [RosterEntryComponent, EmptyRosterEntryComponent, CommonModule],
   templateUrl: './roster.component.html',
   styleUrl: './roster.component.scss',
 })
 export class RosterComponent {
   @Input() gameState!: GameState;
-  positionList = ['QB', 'RB', 'WR', 'TE'];
+  positionList: Position[] = ['QB', 'RB', 'WR', 'TE'];
   rosters: Roster[] = [];
 
   constructor() {}
